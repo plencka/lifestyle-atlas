7c052e034842ca8fdc7c2e346a0735ee24bfec7a
Paulina Lencka <lencka.paulina@gmail.com>
2022-03-16 16:37:00 +0100

    Basic config script

---
+++ composer-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+export MSYS_NO_PATHCONV=1
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v $(pwd):/var/www/html \
+    -w /var/www/html \
+    laravelsail/php81-composer:latest \
+    composer install --ignore-platform-reqs
+read -p "Press any key to continue..."
