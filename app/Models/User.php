f16a66320b992f225a560569c628f4fe483feaad
plencka <lencka.paulina@gmail.com>
2022-05-11 23:16:03 +0200

    PrimaryKey restore.

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
+    protected $primaryKey = 'id';
     /**
      * The attributes that are mass assignable.
      *
